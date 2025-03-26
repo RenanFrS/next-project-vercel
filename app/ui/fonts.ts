94445ec95a21abd5dc219d369df79abf3dee3480
RenanFrS <renanrocha.01@icloud.com>
2025-03-26 11:17:11 -0300

    feat: project vercel

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Tektur } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const tektur = Tektur({ 
+    subsets: ['latin'],
+    weight: ['400', '700'],
+ });
